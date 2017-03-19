f4df392d775f8262ca29b82ddf257facfaaa6329
Krystian Jagoda <kristian2x@gmail.com>
2017-03-19 20:31:09 +0100

    Add sections: eduction, skills, experience. client to provide information

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    //Smooth Scrolling Menu
+    (function() {
+
+        var menu = $('.nav.navbar-nav');
+        var links = menu.find('a');
+
+
+        links.on('click', function(e) {
+            var target = $($(this).attr('href'));
+
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 2000);
+            }
+        });
+    })();
+});
